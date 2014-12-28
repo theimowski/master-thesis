b94698ee556168d2e503406e6de75a7ac7603c80
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2014-12-28 14:40:37 +0100

    Parametrize browse route

--- src/MusicStore/Program.fs
+++ src/MusicStore/Program.fs
@@ -4,11 +4,15 @@ open Suave.Web             // for config
 open Suave.Http
 open Suave.Http.Applicatives
 open Suave.Http.RequestErrors
+open Suave.Types
 
 choose [
     GET >>= choose [
         url "/store" >>= (OK "Hello from store")
-        url "/store/browse" >>= (OK "Hello from browse")
+        url "/store/browse" 
+            >>= request(fun request -> cond (HttpRequest.query(request) ^^ "genre") 
+                                            (fun genre -> OK (sprintf "Genre: %s" genre)) 
+                                            never)
         url "/store/details" >>= (OK "Hello from details")
     ]
 
